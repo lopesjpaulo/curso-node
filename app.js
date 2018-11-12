3ef2fc885d1a739237ec39dcbe58ceb97ba7504f
lopesjpaulo <lopes_jpaulo@hotmail.com>
2018-11-12 17:56:09 -0300

    add views

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res){
-    res.send("<html><body>Portal de noticias</body></html>")
+    res.render('inicial');
 });
 
 app.listen(3000, function(){
