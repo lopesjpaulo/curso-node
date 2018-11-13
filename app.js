ea8d69ea6bea0763b5303b6c9f65b36eb6ad69d8
lopesjpaulo <lopes_jpaulo@hotmail.com>
2018-11-12 22:00:29 -0300

    add config

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-var express = require('express');
-var app = express();
-
-app.set('view engine', 'ejs');
+var app = require('./config/server');
 
 app.get('/', function(req, res){
     res.render('inicial');
 });
 
-app.listen(3000, function(){
+app.get('/form', function(req, res){
+    res.render('admin/form_add_noticia');
+});
 
+app.get('/noticias', function(req, res){
+    res.render('admin/noticias');
+});
+
+app.listen(3000, function(){
+    console.log('Rodando');
 });
